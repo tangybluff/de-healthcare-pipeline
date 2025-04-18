b271f6cddfa3f906ee0d5b6dc3148eb96e122e5e
Ahmed Bouallou <ahmedbouallou@gmail.com>
2025-04-18 22:26:23 +0000

    Bulk commit models

--- models/staging/stg_silver__patients.sql
+++ models/staging/stg_silver__patients.sql
@@ -19,7 +19,7 @@ with
                 when source.clasiffication_final >= 4
                 then 'not_carrier_or_inconclusive'
                 else null
-            end as clasiffication_final,
+            end as classification,
             case
                 when source.patient_type = 1
                 then 'returned_home'
@@ -27,22 +27,52 @@ with
                 then 'hospitalized'
                 else null
             end as patient_type,
-            nullif(source.pneumonia, 99) as pneumonia,
-            nullif(source.pregnant, 99) as pregnancy,
-            nullif(source.diabetes, 99) as diabetes,
-            nullif(source.copd, 99) as copd,
-            nullif(source.asthma, 99) as asthma,
-            nullif(source.inmsupr, 99) as inmsupr,
-            nullif(source.hipertension, 99) as hypertension,
-            nullif(source.cardiovascular, 99) as cardiovascular,
-            nullif(source.renal_chronic, 99) as renal_chronic,
-            nullif(source.other_disease, 99) as other_disease,
-            nullif(source.obesity, 99) as obesity,
-            nullif(source.tobacco, 99) as tobacco,
+            case
+                when source.pneumonia in (99, 97) then null else source.pneumonia
+            end as pneumonia,
+            case
+                when source.pregnant in (99, 97) then null else source.pregnant
+            end as pregnancy,
+            case
+                when source.diabetes in (99, 97) then null else source.diabetes
+            end as diabetes,
+            case when source.copd in (99, 97) then null else source.copd end as copd,
+            case
+                when source.asthma in (99, 97) then null else source.asthma
+            end as asthma,
+            case
+                when source.inmsupr in (99, 97) then null else source.inmsupr
+            end as inmsupr,
+            case
+                when source.hipertension in (99, 97) then null else source.hipertension
+            end as hypertension,
+            case
+                when source.cardiovascular in (99, 97)
+                then null
+                else source.cardiovascular
+            end as cardiovascular,
+            case
+                when source.renal_chronic in (99, 97)
+                then null
+                else source.renal_chronic
+            end as renal_chronic,
+            case
+                when source.other_disease in (99, 97)
+                then null
+                else source.other_disease
+            end as other_disease,
+            case
+                when source.obesity in (99, 97) then null else source.obesity
+            end as obesity,
+            case
+                when source.tobacco in (99, 97) then null else source.tobacco
+            end as tobacco,
             source.usmer,
             source.medical_unit,
-            nullif(source.intubed, 99) as intubed,
-            nullif(source.icu, 99) as icu,
+            case
+                when source.intubed in (99, 97) then null else source.intubed
+            end as intubed,
+            case when source.icu in (99, 97) then null else source.icu end as icu,
             case
                 when source.date_died = '9999-99-99' then null else date_died
             end as date_died
